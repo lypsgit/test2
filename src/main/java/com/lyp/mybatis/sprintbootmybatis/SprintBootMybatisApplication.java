834d678f528580011506dc9b809b9908e41b889a
lypsgit <Ashinlee12345>
2020-05-09 17:58:43 +0800

    create

---
+++ src/main/java/com/lyp/mybatis/sprintbootmybatis/SprintBootMybatisApplication.java
@@ -0,0 +1,13 @@
+package com.lyp.mybatis.sprintbootmybatis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprintBootMybatisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SprintBootMybatisApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lyp/mybatis/sprintbootmybatis/SprintBootMybatisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lyp.mybatis.sprintbootmybatis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SprintBootMybatisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
